1ed8bd7b1ae1c7e6bf5880e803f5ef94f4a824be
value_ga <odr3045@gmail.com>
2025-02-10 23:21:39 +0900

    url-template-clear

--- test_market/test_market/urls.py
+++ test_market/test_market/urls.py
@@ -1,22 +1,15 @@
-"""
-URL configuration for test_market project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from . import views 
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls), # 관리자 페이지
+    path("market_info/",views.market_info, name="market_info"), # 마켓 정보 페이지
+    path("market_create/",views.market_create, name="market_create"), # 마켓 정보 생성 페이지
+    path("market_update/",views.market_update, name="market_update"), # 마켓 정보 수정 페이지
+    path("market_delete/",views.market_delete, name="market_delete"), # 마켓 정보 삭제 페이지
+    # include 함수를 사용하여 다른 앱의 urls.py 파일을 포함
+    path("customers/", include("customers.urls")), # 고객 정보 페이지
+    path("products/",include("products.urls")), # 제품 정보 페이지
 ]
